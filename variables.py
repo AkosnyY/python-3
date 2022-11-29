9eb6d9f76c29626a98b5b4fa4d76fed911d428e1
akosy <develllopeurweb@gmail.com>
2022-11-29 11:46:52 +0100

    Ajout d'un cours sur les variables
    Création du fichier de cours
    Ajout de variables de type nombre entier et chaîne caractères

---
+++ variables.py
@@ -0,0 +1,13 @@
+#l'opération d'affection = permet d'injecter une valeur dans une variables 
+
+my_number1=12230255151
+my_number2= -422854854
+
+print (my_number1)
+print (my_number2)
+
+my_text1 ="Ceci est une chaine des caractères"
+my_text2 = "Ceci est aussi une chaine de caractères "
+
+print(my_text1 )
+print (my_text2)
